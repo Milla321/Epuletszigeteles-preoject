ec4131440bf2d7b0bcd84bdfb8a85d4e679ebec8
Milla321 <71266144+Milla321@users.noreply.github.com>
2020-11-06 02:36:16 +0100

    Add files via upload

---
+++ Épület projekt.py
@@ -0,0 +1,23 @@
+
+# Talián Milla
+# 2020.10.20
+# Esti szoft.
+
+
+
+print("Talián Milla, 2020.10.20, Esti szoftver")
+
+import math
+
+szelesseg=0 
+hosszusag=0
+magassag=0
+szigeteles=0
+
+szelesseg=int(input("Kérem a szélességet: "))
+hosszusag=int(input("Kérem a hosszúságot: "))
+magassag=int(input("Kérem a magasságot: "))
+
+szigeteles=(szelesseg+hosszusag)*magassag*10
+
+print("Szigetelés=", szigeteles, 'm2')
